301458d67bc80dd3aa4275da6f8c73fd63de3f2d
Guisado95 <148809425+Guisado95@users.noreply.github.com>
2025-04-10 17:29:01 -0300

    Create Livro.java

---
+++ Livro.java
@@ -0,0 +1,43 @@
+class Livro {
+    private String titulo;
+    private String autor;
+    private boolean disponivel;
+
+
+    //construtor
+    public Livro (String titulo, String autor, boolean) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.disponivel = true;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String setTitulo (String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public boolean getDisponivel (){
+        return = disponivel;
+    }
+
+    public boolean setDisponivel (boolean disponivel) {
+        if (disponivel = true) {
+            this.disponivel = disponivel;
+        } else {
+            System.out.println ("O livro está indisponível.")
+        }
+    }
+
+
+}
